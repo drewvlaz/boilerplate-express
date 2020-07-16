f587bda2b0d0b065d28c5796663520e1df7ebb19
Drew Vlasnik <314dsv@gmail.com>
2020-07-16 19:38:10 -0400

    completed objective

--- myApp.js
+++ myApp.js
@@ -7,6 +7,11 @@ app.get("/", (req, res) => {
     res.send("Hello Express")
 });
 
+let absolutePath = __dirname + "/views/index.html";
+app.get("/", (req, res) => {
+    res.sendFile(absolutePath)
+});
+
 // --> 7)  Mount the Logger middleware here
 
 
