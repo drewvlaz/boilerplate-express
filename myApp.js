a5ef0299316041e96749822d35dc19934cd51a1a
Drew Vlasnik <314dsv@gmail.com>
2020-07-16 19:32:34 -0400

    completed objective

--- myApp.js
+++ myApp.js
@@ -3,6 +3,10 @@ var express = require('express');
 var app = express();
 console.log("Hello world");
 
+app.get("/", (req, res) => {
+    res.send("Hello Express")
+});
+
 // --> 7)  Mount the Logger middleware here
 
 
