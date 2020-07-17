9b457f2c8f9e3c736dafc78286a5b9239c167be1
Drew Vlasnik <314dsv@gmail.com>
2020-07-16 20:07:15 -0400

    completed objective

--- myApp.js
+++ myApp.js
@@ -12,7 +12,7 @@ app.use(express.static(__dirname + "/public"));
 
 app.get("/json", (req, res) => {
     let response = "Hello json";
-    if (process.env.MESSAGE_SYTLE === "uppercase") {
+    if (process.env.MESSAGE_SYTLE == "uppercase") {
         response = "Hello json".toUpperCase();
     }
     res.json({
